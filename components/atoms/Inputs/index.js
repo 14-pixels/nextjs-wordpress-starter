c639196a3b9782e7bd31d81922d6f89e063db5e6
Joscha <>
2021-06-30 16:32:14 +0200

    removed not needed compontents

--- components/atoms/Inputs/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Checkbox} from './Checkbox'
-export {default as CheckboxGroup} from './CheckboxGroup'
-export {default as Select} from './Select'
-export {default as Text} from './Text'
